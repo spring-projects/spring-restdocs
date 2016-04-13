032b7b59dc10da72830d4b9c32f20c5876a12a6a
Andy Wilkinson <awilkinson@pivotal.io>
2016-04-13 14:09:08 +0100

    Fix HalLinkExtractor's extraction of link hrefs
    Closes gh-220

--- spring-restdocs-core/src/test/java/org/springframework/restdocs/hypermedia/LinkExtractorsPayloadTests.java
+++ spring-restdocs-core/src/test/java/org/springframework/restdocs/hypermedia/LinkExtractorsPayloadTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 the original author or authors.
+ * Copyright 2014-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ public class LinkExtractorsPayloadTests {
 
 	private final String linkType;
 
-	@Parameters
+	@Parameters(name = "{1}")
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[] { new HalLinkExtractor(), "hal" },
 				new Object[] { new AtomLinkExtractor(), "atom" });
