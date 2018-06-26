spring-projects/spring-restdocs

commit 7476562d221558aaa4fbebdf06bbcae54251b8b0

Consider optional empty arrays to be absent when finding missing fields
Closes gh-519

spring-restdocs-core/src/main/java/org/springframework/restdocs/payload/JsonContentHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 the original author or authors.
+ * Copyright 2014-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,12 +18,15 @@ package org.springframework.restdocs.payload;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.springframework.restdocs.payload.JsonFieldProcessor.ExtractedField;
 
 /**
  * A {@link ContentHandler} for JSON content.
@@ -70,11 +73,26 @@ class JsonContentHandler implements ContentHandler {
 		for (FieldDescriptor candidate : candidates) {
 			if (candidate.isOptional()
 					&& missing.getPath().startsWith(candidate.getPath())
-					&& !this.fieldProcessor.hasField(candidate.getPath(), payload)) {
+					&& isMissing(candidate, payload)) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	private boolean isMissing(FieldDescriptor candidate, Object payload) {
+		try {
+			ExtractedField extracted = this.fieldProcessor.extract(candidate.getPath(),
+					payload);
+			if (extracted.getValue() instanceof Collection
+					&& ((Collection<?>) extracted.getValue()).isEmpty()) {
+				return true;
+			}
+			return false;
+		}
+		catch (FieldDoesNotExistException ex) {
+			return true;
+		}
 	}
 
 	@Override

spring-restdocs-core/src/test/java/org/springframework/restdocs/payload/JsonContentHandlerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 the original author or authors.
+ * Copyright 2014-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -144,6 +144,16 @@ public class JsonContentHandlerTests {
 				"{\"b\":\"bravo\"}".getBytes()).findMissingFields(
 						Arrays.asList(new FieldDescriptor("a").optional(),
 								new FieldDescriptor("b"), new FieldDescriptor("a.c")));
+		assertThat(missingFields.size(), is(equalTo(0)));
+	}
+
+	@Test
+	public void describedFieldThatIsNotPresentNestedBeneathOptionalArrayThatIsEmptyIsNotConsideredMissing() {
+		List<FieldDescriptor> missingFields = new JsonContentHandler(
+				"{\"outer\":[]}".getBytes())
+						.findMissingFields(Arrays.asList(new FieldDescriptor("outer"),
+								new FieldDescriptor("outer[]").optional(),
+								new FieldDescriptor("outer[].inner")));
 		assertThat(missingFields.size(), is(equalTo(0)));
 	}
 

commit 7bde04ea461f1e9e06b93c130e5155cd4230e8ae

Consider sometimes present field beneath optional array to not be missing
The changes made in 7476562d inadvertently caused a field that is
an descendant of an array and a child of an optional field to be
considered missing when its parent is only sometimes present. Due to
the parent being optional, the parent’s absence should not cause the
absent child to be considered to be missing.

Closes gh-519

spring-restdocs-core/src/main/java/org/springframework/restdocs/payload/JsonContentHandler.java
@@ -81,6 +81,9 @@ class JsonContentHandler implements ContentHandler {
 	}
 
 	private boolean isMissing(FieldDescriptor candidate, Object payload) {
+		if (!this.fieldProcessor.hasField(candidate.getPath(), payload)) {
+			return true;
+		}
 		try {
 			ExtractedField extracted = this.fieldProcessor.extract(candidate.getPath(),
 					payload);

spring-restdocs-core/src/test/java/org/springframework/restdocs/payload/JsonContentHandlerTests.java
@@ -157,4 +157,14 @@ public class JsonContentHandlerTests {
 		assertThat(missingFields.size(), is(equalTo(0)));
 	}
 
+	@Test
+	public void describedFieldThatIsSometimesPresentChildOfOptionalArrayIsNotConsideredMissing() {
+		List<FieldDescriptor> missingFields = new JsonContentHandler(
+				"{\"a\":[ {\"b\": \"bravo\"}, {\"b\": \"bravo\", \"c\": { \"d\": \"delta\"}}]}"
+						.getBytes()).findMissingFields(
+								Arrays.asList(new FieldDescriptor("a.[].c").optional(),
+										new FieldDescriptor("a.[].c.d]")));
+		assertThat(missingFields.size(), is(equalTo(0)));
+	}
+
 }