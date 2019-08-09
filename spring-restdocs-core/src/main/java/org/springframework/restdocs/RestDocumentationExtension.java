c0930a48c0cbeffb3832c8250553412a951843a5
Jochen Just <jochen.just@novatec-gmbh.de>
2019-08-09 12:38:12 +0200

    Allow custom snippets directory via JUnit 5

--- spring-restdocs-core/src/main/java/org/springframework/restdocs/RestDocumentationExtension.java
+++ spring-restdocs-core/src/main/java/org/springframework/restdocs/RestDocumentationExtension.java
@@ -33,6 +33,16 @@ import org.junit.jupiter.api.extension.ParameterResolver;
 public class RestDocumentationExtension
 		implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
 
+	private final String outputDirectory;
+
+	public RestDocumentationExtension () {
+		this(null);
+	}
+
+	public RestDocumentationExtension (String outputDirectory) {
+		this.outputDirectory = outputDirectory;
+	}
+
 	@Override
 	public void beforeEach(ExtensionContext context) throws Exception {
 		this.getDelegate(context).beforeTest(context.getRequiredTestClass(),
@@ -68,8 +78,15 @@ public class RestDocumentationExtension
 	private ManualRestDocumentation getDelegate(ExtensionContext context) {
 		Namespace namespace = Namespace.create(getClass(), context.getUniqueId());
 		return context.getStore(namespace).getOrComputeIfAbsent(
-				ManualRestDocumentation.class, (key) -> new ManualRestDocumentation(),
+				ManualRestDocumentation.class, this::createManualRestDocumentation,
 				ManualRestDocumentation.class);
 	}
 
+	private ManualRestDocumentation createManualRestDocumentation (Class<ManualRestDocumentation> key) {
+		if (outputDirectory != null) {
+			return new ManualRestDocumentation(outputDirectory);
+		} else {
+			return new ManualRestDocumentation();
+		}
+	}
 }
