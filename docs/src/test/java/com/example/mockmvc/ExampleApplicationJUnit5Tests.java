07a6f2675535b9fe4938deb620e110cc25622f37
izeye <izeye@naver.com>
2022-05-18 19:46:12 +0900

    Polish
    See gh-808

--- docs/src/test/java/com/example/mockmvc/ExampleApplicationJUnit5Tests.java
+++ docs/src/test/java/com/example/mockmvc/ExampleApplicationJUnit5Tests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,14 +28,14 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
 @ExtendWith(RestDocumentationExtension.class)
-public class ExampleApplicationJUnit5Tests {
+class ExampleApplicationJUnit5Tests {
 
 	@SuppressWarnings("unused")
 	// tag::setup[]
 	private MockMvc mockMvc;
 
 	@BeforeEach
-	public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+	void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
 				.apply(documentationConfiguration(restDocumentation)) // <1>
 				.build();
--- docs/src/test/java/com/example/webtestclient/ExampleApplicationJUnit5Tests.java
+++ docs/src/test/java/com/example/webtestclient/ExampleApplicationJUnit5Tests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,14 +27,14 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 
 @ExtendWith(RestDocumentationExtension.class)
-public class ExampleApplicationJUnit5Tests {
+class ExampleApplicationJUnit5Tests {
 
 	@SuppressWarnings("unused")
 	// tag::setup[]
 	private WebTestClient webTestClient;
 
 	@BeforeEach
-	public void setUp(ApplicationContext applicationContext, RestDocumentationContextProvider restDocumentation) {
+	void setUp(ApplicationContext applicationContext, RestDocumentationContextProvider restDocumentation) {
 		this.webTestClient = WebTestClient.bindToApplicationContext(applicationContext).configureClient()
 				.filter(documentationConfiguration(restDocumentation)) // <1>
 				.build();
