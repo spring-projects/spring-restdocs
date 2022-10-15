4807bec4b78b58536177775494025f1020021fcb
Andy Wilkinson <wilkinsona@vmware.com>
2022-10-15 16:13:25 +0100

    Remove samples in favor of those in separate repository
    Closes gh-852

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -5,5 +5,5 @@ source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
 
 pushd git-repo > /dev/null
-./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} build buildSamples publishAllPublicationsToDeploymentRepository
+./gradlew -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} build publishAllPublicationsToDeploymentRepository
 popd > /dev/null
