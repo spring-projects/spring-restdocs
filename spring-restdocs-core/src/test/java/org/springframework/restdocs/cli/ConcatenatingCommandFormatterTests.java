79ff75fe6eca5ae4ee9daa12c361f5e498360e65
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-01-24 14:11:13 +0000

    Polish "Remove unused String.format call"
    See gh-916

--- spring-restdocs-core/src/test/java/org/springframework/restdocs/cli/ConcatenatingCommandFormatterTests.java
+++ spring-restdocs-core/src/test/java/org/springframework/restdocs/cli/ConcatenatingCommandFormatterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 the original author or authors.
+ * Copyright 2014-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,8 +50,7 @@ public class ConcatenatingCommandFormatterTests {
 
 	@Test
 	public void formattingMultipleElements() {
-		assertThat(this.singleLineFormat.format(Arrays.asList("alpha", "bravo")))
-			.isEqualTo(" alpha bravo");
+		assertThat(this.singleLineFormat.format(Arrays.asList("alpha", "bravo"))).isEqualTo(" alpha bravo");
 	}
 
 }
