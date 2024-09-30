694f28e0303d964f141e6ee2663837fddc566cf6
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-09-30 11:20:23 +0100

    Upgrade to Spring Java Format 0.0.43

--- spring-restdocs-core/src/main/java/org/springframework/restdocs/payload/JsonFieldPath.java
+++ spring-restdocs-core/src/main/java/org/springframework/restdocs/payload/JsonFieldPath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 the original author or authors.
+ * Copyright 2014-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,7 +153,7 @@ final class JsonFieldPath {
 		/**
 		 * The path identifies multiple items in the payload.
 		 */
-		MULTI;
+		MULTI
 
 	}
 
