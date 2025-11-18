4e9d464770d62c162f9e58598ee9d8522bc6660d
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-11-18 10:21:40 +0000

    Restructure the documentation into smaller sections
    See gh-1009

---
+++ spring-restdocs-docs/src/test/java/org/springframework/restdocs/docs/howto/parameterizingtheoutputdirectory/webtestclient/ParameterizedOutput.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.restdocs.docs.howto.parameterizingtheoutputdirectory.webtestclient;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
+
+@ExtendWith(RestDocumentationExtension.class)
+class ParameterizedOutput {
+
+	// @fold:on // Fields
+	@SuppressWarnings("unused")
+	private WebTestClient webTestClient;
+
+	@Autowired
+	private ApplicationContext context;
+
+	// @fold:off
+
+	@BeforeEach
+	void setUp(RestDocumentationContextProvider restDocumentation) {
+		this.webTestClient = WebTestClient.bindToApplicationContext(this.context)
+			.configureClient()
+			.filter(documentationConfiguration(restDocumentation))
+			.entityExchangeResultConsumer(document("{method-name}/{step}"))
+			.build();
+	}
+
+}
---
+++ spring-restdocs-docs/src/test/java/org/springframework/restdocs/docs/howto/reusingsnippets/webtestclient/ReusingSnippets.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.restdocs.docs.howto.reusingsnippets.webtestclient;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.docs.howto.reusingsnippets.CommonSnippets;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+
+public class ReusingSnippets {
+
+	// @fold:on // Fields
+	private WebTestClient webTestClient;
+
+	// @fold:off
+
+	@Test
+	void test() {
+		this.webTestClient.get()
+			.uri("/")
+			.accept(MediaType.APPLICATION_JSON)
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody()
+			.consumeWith(document("example", CommonSnippets.pagingLinks.and(// <1>
+					linkWithRel("alpha").description("Link to the alpha resource"),
+					linkWithRel("bravo").description("Link to the bravo resource"))));
+	}
+
+}
